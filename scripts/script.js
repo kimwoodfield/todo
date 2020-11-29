abff60e67e00be4d67c65fe409a8add9ae542aa1
kimwoodfield <kimwoodfield@live.com>
2020-11-29 18:12:13 +1000

    checking list item types

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ function inputLength() {
 }
 
 // if the input length is more than zero, a new list item can be added.
-function addListAfterClick() {
+function addItemAfterClick() {
     if (inputLength() > 1) {
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(input.value));
@@ -23,12 +23,33 @@ function addListAfterClick() {
     }
 }
 
+// Checks the list item types of the todo list
+// function checkListTypes() {
+//     // grabs the elements currently inside the unordered list element
+//     let listItems = document.querySelector("#list").children;
+//     let liElems = [];
+//     // creates a variable of 'listItem' for each item in the list
+//     for (const listItem of listItems) {
+//         // 
+//         if (listItem.tagName === "LI") {
+//             liElems.push("listItem");
+//             return liElems.length;
+//         }
+//     }
+
+// }
+
+
+
+
+
+// Event Listeners
 
 // Handles adding a new list item
 add.addEventListener("click", function() {
 let totalTodos = document.querySelector("#list").children.length;
 if (totalTodos < 5) {
-    addListAfterClick();
+    addItemAfterClick();
 } else {
     if (error.children.length === 0) {
         var errorText = document.createElement("p");
@@ -48,29 +69,3 @@ if (totalTodos < 5) {
 // Must be able to delete items from the list
 // Must be able to change items on the list
 // Must be able to delete ALL items from the list
-
-
-
-// Handles adding a new list item
-// add.addEventListener("click", function() {
-//     let totalTodos = document.querySelector("#list").children.length;
-//     if (inputLength() > 1) {
-//         if (totalTodos < 5) {
-//             // create the list item
-//             var li = document.createElement("li");
-//             li.appendChild(document.createTextNode(input.value));
-//             list.appendChild(li);
-//             input.value = "";
-//             console.log("click is working");
-//         } else {
-//             if (error.children.length === 0) {
-//                 var errorText = document.createElement("p");
-//                 errorText.appendChild(document.createTextNode("You can only have 5 todo items"));
-//                 error.appendChild(errorText);
-//                 error.style.backgroundColor = "red";
-//             } else {
-//                 console.log("there are too many todos");
-//             }        
-//         }
-//     }
-//     });
