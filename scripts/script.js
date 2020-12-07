d7eedfd31c0ca2d19361608e3c5abe9dded01edf
Kim Woodfield <kimwoodfield@live.com>
2020-12-08 07:55:21 +1000

    separating addtodo functions

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,8 @@ add.addEventListener("click", checkInputLength);
 
 
 // Returns the length of the input
-function checkInputLength(userinput) {
-    console.log(userinput.length);
+function checkInputLength() {
+    console.log(userinput.value.length);
 }
 
 
@@ -16,25 +16,28 @@ function clearUserInput() {
 }
 
 
-// Creates a new list item with an edit and delete button attached.
-function addListItems() {
-
-    // Handles creating a new list item
+// Handles creating a new list item
+function addListItem() {
     const newLI = document.createElement("LI");
     const textnode = document.createTextNode(userinput.value);
     newLI.appendChild(textnode);
     document.getElementById("list").appendChild(newLI);
+}
 
-    // Handles creating a new edit button for the list item
+
+// Handles creating a new edit button for the list item
+function addDeleteBtn() {
     const editButton = document.createElement("button");
     const editButtonText = document.createTextNode("Edit");
     editButton.appendChild(editButtonText);
     document.getElementById("list").appendChild(editButton);
+}
 
-    // Handles creating a new delete button for the list item
+
+// Handles creating a new delete button for the list item
+function addEditBtn() {
     const deleteButton = document.createElement("button");
     const deleteButtonText = document.createTextNode("Delete");
     deleteButton.appendChild(deleteButtonText);
     document.getElementById("list").appendChild(deleteButton);
-
 }
